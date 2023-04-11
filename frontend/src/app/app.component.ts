27cb40e02471451c0881fdbb44081b8f5e66d70b
Nils Tijtgat <nilstijtgat@gmail.com>
2023-04-11 14:18:22 +0200

    Improve UI

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { animations, ccControls, colors, positions } from '../../../data';
 
 @Component({
@@ -12,20 +13,15 @@ export class AppComponent implements OnInit {
     readonly colors = colors;
     readonly positions = positions;
 
+    midiDevicesControl = new FormControl<WebMidi.MIDIOutput[]>([]);
+
     midiNoteStatusMap: Record<number, boolean> = {};
-    currentMidiDevice: WebMidi.MIDIOutput | undefined;
     midiOutputDevices: WebMidi.MIDIOutput[] = [];
     title = 'frontend';
 
     ngOnInit(): void {
         window.navigator.requestMIDIAccess().then((access) => {
             this.midiOutputDevices = Array.from(access.outputs.values());
-
-            if (this.midiOutputDevices.length > 0) {
-                this.currentMidiDevice = this.midiOutputDevices[0];
-            }
-
-            console.log(this.currentMidiDevice);
 
             access.onstatechange = (e) => {
                 // Print information about the (dis)connected MIDI controller
@@ -35,31 +31,19 @@ export class AppComponent implements OnInit {
     }
 
     toggleMidi(noteNumber: number) {
-        if (!this.currentMidiDevice) {
-            return;
-        }
+        (this.midiDevicesControl.value ?? []).forEach((midiOutputDevice) => {
+            midiOutputDevice.send([this.midiNoteStatusMap[noteNumber] ? 0x80 : 0x90, noteNumber, 0x7f]);
+        });
 
-        console.log();
-
-        this.currentMidiDevice.send([this.midiNoteStatusMap[noteNumber] ? 0x80 : 0x90, noteNumber, 0x7f]);
         this.midiNoteStatusMap[noteNumber] = Boolean(!this.midiNoteStatusMap[noteNumber]);
     }
 
     changeCCValue(channel: number, event: Event) {
         if (event.target && event.target instanceof HTMLInputElement) {
-            if (!this.currentMidiDevice) {
-                return;
-            }
-
-            const value = parseInt(event.target.value, 10);
-            this.currentMidiDevice.send([0xb0, channel, value]);
-        }
-    }
-
-    changeMidiDevice(event: Event): void {
-        if (event.target && event.target instanceof HTMLSelectElement && typeof event.target.value) {
-            const id = event.target.value;
-            this.currentMidiDevice = this.midiOutputDevices.find((device) => device.id === id);
+            const value = Math.round((parseInt(event.target.value, 10) / 100) * 127);
+            (this.midiDevicesControl.value ?? []).forEach((midiOutputDevice) => {
+                midiOutputDevice.send([0xb0, channel, value]);
+            });
         }
     }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatSliderModule,
+        ReactiveFormsModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
